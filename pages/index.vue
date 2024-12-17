3c8ea8799346d7f9da3a9d083b52cc7ec595afc9
Boaz <webevolution13@gmail.com>
2024-12-17 16:53:31 +0100

    upDate
    about

--- pages/index.vue
+++ pages/index.vue
@@ -152,4 +152,41 @@
       </div>
     </div>
   </section>
+  <section>
+    <div class="about-content position-relative d-flex">
+      <div class="about-content__box"></div>
+      <div class="about-content__iBox">
+        <NuxtImg
+          src="/img/body/about.webp"
+          alt="Műszaki vizsgáztatás"
+          class="about-content__iBox__img"
+          height="100%"
+        />
+      </div>
+      <div class="about-content__tBox position-absolute bg-color-w">
+        <h4 class="about-content__tBox__h4 text-transform-uppercase">Rólunk</h4>
+        <p class="about-content__tBox__p">
+          Több mint 66 éve segítjük az autósokat megbízható és professzionális
+          szolgáltatásokkal. Szervizünk minden járműtípust fogad, legyen szó
+          műszaki vizsgáztatásról, eredetiségvizsgálatról, kisebb javításokról
+          vagy komolyabb szerelésekről. 15 fős szakértői csapatunk elkötelezett
+          abban, hogy gyorsan és a lehető legjobb megoldásokkal szolgáljuk ki
+          ügyfeleinket, márkafüggetlen szemlélettel.
+        </p>
+        <p class="about-content__tBox__p">
+          Célunk, hogy az autójával kapcsolatos teendők nálunk egyszerűvé és
+          gördülékennyé váljanak. Modern környezetben, kényelmes várakozási
+          lehetőséggel, barátságos kiszolgálással és ügyfélközpontú
+          hozzáállással várjuk Önt, mert számunkra az elégedettsége a
+          legfontosabb. Bízunk benne, hogy hamarosan Önt is ügyfeleink között
+          köszönthetjük!
+        </p>
+        <NuxtLink
+          to="/"
+          class="about-content__tBox__link page-link text-color-w"
+          >Tudjon meg többet rólunk</NuxtLink
+        >
+      </div>
+    </div>
+  </section>
 </template>
