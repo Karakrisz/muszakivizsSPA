d526c4610b79320d05c4e50bd01e202f9015d059
Boaz <webevolution13@gmail.com>
2025-02-06 19:32:20 +0100

    upDate
    front end

--- pages/ajanlatkeres.vue
+++ pages/ajanlatkeres.vue
@@ -5,5 +5,43 @@ useHead({
 </script>
 
 <template>
-  <div class="subpage-content subpage-footer-content"></div>
+  <section>
+    <div class="about-content about-content--subpage-next-format position-relative">
+      <div class="subpage-content">
+        <h2 class="page-color">
+          ÍRJON NEKÜNK, HAMAR VÁLASZOLUNK
+        </h2>
+        <p class="page-color">
+          Amennyiben bármilyen kérdése merülne fel, ne habozzon kapcsolatba
+          lépni velünk! Írjon nekünk bátran, és csapatunk igyekszik a lehető
+          legrövidebb időn belül válaszolni minden érdeklődésre. Várjuk, hogy
+          halljunk Ön felől!
+        </p>
+        <h2 class="page-color">
+          Kapcsolatfelvételi űrlap:
+        </h2>
+        <ul class="supage-content__ul">
+          <li class="page-color">
+            <strong class="supage-content__ul__li__strong">Név:</strong> Kérjük,
+            adja meg a nevét, hogy személyre szabottan válaszolhassunk.
+          </li>
+          <li class="page-color">
+            <strong class="supage-content__ul__li__strong">Email cím:</strong>
+            Ide küldjük a válaszunkat és minden további információt.
+          </li>
+          <li class="page-color">
+            <strong class="supage-content__ul__li__strong">Telefonszám:</strong
+            >Ha telefonon szeretne kapcsolatba lépni, kérjük, adja meg
+            telefonszámát.
+          </li>
+        </ul>
+        <p class="page-color">
+          <i class="supage-content__p__i"
+            >A küldés gombra kattintva automatikusan elfogadja az Adatvédelmi
+            Szabályzatot.
+          </i>
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
