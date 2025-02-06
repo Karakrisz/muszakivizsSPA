b286e3ce92e4854d0471c621af42079a47d22e22
Boaz <webevolution13@gmail.com>
2025-02-06 20:41:17 +0100

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
         src="/img/body/hero.svg"
         alt="Műszaki vizsgáztatás"
         class="hero-content__img"
-        width="1920"
-        height="650"
+        width="100%"
+        height="100%"
         format="svg"
         sizes="100vw"
       />
