003c1322280f60d1c78d078a37a908165fd9ee61
Boaz <webevolution13@gmail.com>
2025-05-23 11:12:33 +0200

    upDate
    front end

--- pages/rolunk.vue
+++ pages/rolunk.vue
@@ -25,7 +25,7 @@ useHead({
           Rólunk
         </h4>
         <p class="about-content__tBox__p">
-          Több mint 66 éve segítjük az autósokat megbízható és professzionális
+          Több mint 77 éve segítjük az autósokat megbízható és professzionális
           szolgáltatásokkal. Szervizünk minden járműtípust fogad, legyen szó
           műszaki vizsgáztatásról, eredetiségvizsgálatról, kisebb javításokról
           vagy komolyabb szerelésekről. 15 fős szakértői csapatunk elkötelezett
